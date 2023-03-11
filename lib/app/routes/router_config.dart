d734afac346a1bf19fcb665c8f0cff2d498ab4ca
Max Weber <max.weber@sonomotors.com>
2023-03-11 20:50:15 +0100

    [Configurations] Added support for Firebase Project regarding the environment it is build for iOS.

--- lib/app/routes/router_config.dart
+++ lib/app/routes/router_config.dart
@@ -57,7 +57,7 @@ final GoRouter routerConfig = GoRouter(
         actions: [
           AuthStateChangeAction((context, state) {
             context.go('/profile');
-          })
+          }),
         ],
       ),
     ),
